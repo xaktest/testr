0922e4d782202dd3444aaebf094e927d066cf044
xaktest <xaktest@bk.ru>
2014-01-28 13:03:51 +0200

    stest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialNetwork::Application.config.secret_key_base = '3d340867c862a1e515b95d5792593c40de445cc5e4d299580dfeb182a6d8d94d88e0cfc642af886c20836a84f0ab083219e01f1b66a47c799cd42ee3a61da4b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialNetwork::Application.config.session_store :cookie_store, key: '_Social-network_session'
