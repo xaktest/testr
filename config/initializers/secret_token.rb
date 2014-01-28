05ce6840688a871ab001cc3308b19ab38dcf0f04
xaktest <xaktest@bk.ru>
2014-01-28 10:50:47 +0200

    testr_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testr::Application.config.secret_key_base = 'd6bff9b52a79dbd2d458778611498ca7b633f11a97b2a56ecbb64f32ad17dd042772ce3165305e4bb9e0d10af6cde23bc5f7527d6f198c37d358b98c5b60cc8c'
